ba303c2c9952f58090fc6dda37c046047e124212
oskar <oskar.ruuder@metropolia.fi>
2025-08-27 22:45:12 +0300

    commit

---
+++ Moduuli 02 tehtävät/Tehtävä 2.5.py
@@ -0,0 +1,12 @@
+leiviskat = float(input("anna leiviskät: "))
+luodit = float(input("anna luodit: " ))
+naulat = float(input("anna naulat: "))
+massa = leiviskat + luodit + naulat
+leiviskat = 8512
+luodit = 13,3
+naulat = 425,6
+print("Massa on: ", massa)
+
+
+
+
