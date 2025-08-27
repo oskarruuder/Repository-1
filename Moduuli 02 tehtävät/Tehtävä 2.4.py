c15bde9966f86100358df1f9d5836ee221b08464
oskar <oskar.ruuder@metropolia.fi>
2025-08-27 21:57:46 +0300

    commit

---
+++ Moduuli 02 tehtävät/Tehtävä 2.4.py
@@ -0,0 +1,10 @@
+Luku1 = int(input("Ensimmäinen luku: "))
+Luku2 = int(input("Toinen luku: "))
+Luku3 = int(input("Kolmas luku: "))
+summa = Luku1 + Luku2 + Luku3
+print("Summa on: ", summa)
+tulo = Luku1 * Luku2 * Luku3
+print("Tulo on: ", tulo)
+keskiarvo = summa / 3
+print("Keskiarvo on: ", keskiarvo)
+
