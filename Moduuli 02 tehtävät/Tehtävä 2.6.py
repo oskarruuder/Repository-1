03b286ba2aef1f4c0a8427d89ed33627e5926d47
oskar <oskar.ruuder@metropolia.fi>
2025-08-30 09:27:59 +0300

    commit

---
+++ Moduuli 02 tehtävät/Tehtävä 2.6.py
@@ -0,0 +1,17 @@
+import random
+random.randint(1, 9)
+a = random.randint(1, 9)
+b = random.randint(1, 9)
+c = random.randint(1, 9)
+
+d = random.randint(1, 6)
+e = random.randint(1, 6)
+f = random.randint(1, 6)
+g = random.randint(1, 6)
+
+print(a, b, c)
+
+print(d, e, f, g)
+
+
+
